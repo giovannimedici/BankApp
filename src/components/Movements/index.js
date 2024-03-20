d0639ee977ac1c30a37f6b5199167f70185b8c07
giovannimedici <giovannigilsu@gmail.com>
2024-03-20 20:18:53 -0300

    add menu buttons

--- src/components/Movements/index.js
+++ src/components/Movements/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { View, 
     StyleSheet, 
     Text,
@@ -5,16 +6,21 @@ import { View,
 } from 'react-native';
 
 export default function Movements({data}) {
+    const [showValue, setShowValue] = useState(false);
+
  return (
-   <TouchableOpacity style={styles.container}>
+   <TouchableOpacity style={styles.container} onPress={ () => setShowValue(!showValue) }>
         <Text style={styles.date}>{data.date}</Text>
 
         <View style={styles.content}>
             <Text style={styles.label}>{data.label}</Text>
 
+            {showValue ? (
             <Text style={data.type === 1 ? styles.value : styles.expenses}>
                 {data.type === 1 ? `R$ ${data.value}`:  `R$ -${data.value}`}
-            </Text>
+            </Text>) : (
+                <View style={styles.skeleton}></View>
+            )}
         </View>
    </TouchableOpacity>
   );
@@ -50,5 +56,12 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#e74c3c',
         fontWeight: 'bold'
+    },
+    skeleton:{
+        marginTop: 6,
+        width: 80,
+        height: 10,
+        backgroundColor: "#DADADA",
+        borderRadius: 0,
     }
 })
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
 import Balance from '../../components/Balance';
 import Movements from '../../components/Movements';
+import Actions from '../../components/Actions';
 
 const list = [
     {
@@ -35,6 +36,8 @@ export default function Home() {
 
         <Balance saldo="9.250,90" gastos="-527,00"/>
 
+        <Actions/>
+
         <Text style={styles.title}>Últimas movimentações</Text>
 
         <FlatList
@@ -56,9 +59,7 @@ const styles = StyleSheet.create({
   title:{
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 14,
-    marginRight: 14,
-    marginTop: 14,
+    margin: 14
   },
   list:{
     marginLeft: 14
